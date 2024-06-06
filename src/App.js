e9d5573088a198c29272dcdc2a52f115d08f69da
堀合裕太 <sub.juta@gmail.com>
2024-06-06 22:53:06 +0900

    課題１
    - Hello Reactする
    - 任意のリンクを設定してみる

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function Heading() {
+  return (
+    <div className="Heading">
+    <h1>Hello React!!</h1>
+    </div>
+  );
+}
+
+export default Heading;
